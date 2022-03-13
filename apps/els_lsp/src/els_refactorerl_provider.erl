f86b4e498dbec4e7990638de07a376f3620df8fd
Robert Fiko <64466886+robertfiko@users.noreply.github.com>
2022-03-13 16:12:48 +0100

    First run for it

--- apps/els_lsp/src/els_refactorerl_provider.erl
+++ apps/els_lsp/src/els_refactorerl_provider.erl
@@ -1,4 +1,4 @@
--module(els_code_action_provider).
+-module(els_refactorerl_provider).
 
 -behaviour(els_provider).
 
@@ -52,7 +52,7 @@ replace_lines_action(Uri, Title, Kind, Lines, Range) ->
    , kind => Kind
    , command =>
        els_command:make_command( Title
-                               , <<"replace-lines">>
+                               , <<"replace-lines-r">>
                                , [#{ uri   => Uri
                                    , lines => Lines
                                    , from  => StartLine
@@ -62,7 +62,7 @@ replace_lines_action(Uri, Title, Kind, Lines, Range) ->
 -spec make_code_action(uri(), els_diagnostics:diagnostic()) -> [map()].
 make_code_action(Uri, #{ <<"message">> := Message
                        , <<"range">>   := Range } = _Diagnostic) ->
-  unused_variable_action(Uri, Range, Message) ++ unused_variable_actionR(Uri, Range, Message).
+  unused_variable_action(Uri, Range, Message).
 
 %%------------------------------------------------------------------------------
 
@@ -99,48 +99,7 @@ make_unused_variable_action(Uri, Range, UnusedVariable) ->
   end,
   UpdatedLine = lists:flatten(lists:map(Replace, Tokens)) ++ "\n",
     [ replace_lines_action( Uri
-                      , <<"Add  a '_' to '", UnusedVariable/binary, "'">>
-                      , ?CODE_ACTION_KIND_QUICKFIX
-                      , els_utils:to_binary(UpdatedLine)
-                      , Range)].
-
-%%------------------------------------------------------------------------------
-%% REFERL
-
--spec unused_variable_actionR(uri(), range(), binary()) -> [map()].
-unused_variable_actionR(Uri, Range, Message) ->
-  %% Processing messages like "variable 'Foo' is unused"
-  case re:run(Message, "variable '(.*)' is unused"
-             , [{capture, all_but_first, binary}]) of
-      {match, [UnusedVariable]} ->
-          make_unused_variable_actionR(Uri, Range, UnusedVariable);
-      _ -> []
-  end.
-
--spec make_unused_variable_actionR(uri(), range(), binary()) -> [map()].
-make_unused_variable_actionR(Uri, Range, UnusedVariable) ->
-  #{ <<"start">> := #{ <<"character">> := _StartCol
-                     , <<"line">>      := StartLine }
-   , <<"end">>   := _End
-   } = Range,
-  %% processing messages like "variable 'Foo' is unused"
-  {ok, #{text := Bin}} = els_utils:lookup_document(Uri),
-  Line = els_utils:to_list(els_text:line(Bin, StartLine)),
-
-  {ok, Tokens, _} = erl_scan:string(Line, 1, [return, text]),
-  UnusedString = els_utils:to_list(UnusedVariable),
-  Replace =
-        fun(Tok) ->
-            case Tok of
-                {var, [{text, UnusedString}, _], _} -> "_" ++ UnusedString;
-                {var, [{text, VarName}, _], _} -> VarName;
-                {_,   [{text, Text   }, _], _} -> Text;
-                {_,   [{text, Text   }, _]}    -> Text
-            end
-  end,
-  UpdatedLine = lists:flatten(lists:map(Replace, Tokens)) ++ "\n",
-    [ replace_lines_action( Uri
-                      , <<"Add ccc  a '_' to '", UnusedVariable/binary, "'">>
+                      , <<"Add XX '_' to '", UnusedVariable/binary, "'">>
                       , ?CODE_ACTION_KIND_QUICKFIX
                       , els_utils:to_binary(UpdatedLine)
                       , Range)].
